aa118b67b85e86e7d198fedbb86c48e1ac69bb15
Rafael Belo <rafaelrpbelo@gmail.com>
2020-01-25 12:24:49 -0300

    Enable set 'force_ssl' and 'host_url' by env var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,11 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true if ENV.fetch('FORCE_SSL', false)
+
+  if ENV['HOST_URL']
+    Rails.application.routes.default_url_options[:host] = ENV['HOST_URL']
+  end
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
