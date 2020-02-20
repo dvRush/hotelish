78c13bf88652795df8f7ad210df8aae74ad3a408
Rafael Belo <rafaelrpbelo@gmail.com>
2020-02-19 22:39:26 -0300

    Add form declaration at admin/accommodations.rb
    Add declaration to avoid user input `deleted_at` field that was
    automatically added.

--- app/admin/accommodations.rb
+++ app/admin/accommodations.rb
@@ -14,5 +14,16 @@ ActiveAdmin.register Accommodation do
     actions
   end
 
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :description
+      f.input :type
+      f.input :daily_value
+      f.input :available
+    end
+    f.actions
+  end
+
   permit_params :title, :description, :type, :daily_value, :available
 end
