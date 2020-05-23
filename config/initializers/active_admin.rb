3e9118bbdaff1344794073c35888a522e5ec3355
Rafael Belo <rafaelrpbelo@gmail.com>
2020-05-23 00:51:01 -0300

    Disable notes menu from active admin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -129,10 +129,10 @@ ActiveAdmin.setup do |config|
   # config.comments_order = 'created_at ASC'
   #
   # You can disable the menu item for the comments index page:
-  # config.comments_menu = false
+  config.comments_menu = false
   #
   # You can customize the comment menu:
-  config.comments_menu = { priority: 9999 }
+  # config.comments_menu = { priority: 9999 }
 
   # == Batch Actions
   #
