de4e22687e216771e07d8e320c41aa5747406cc0
Rafael Belo <rafaelrpbelo@gmail.com>
2020-06-21 10:54:36 -0300

    Enable SMTP email sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,16 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['SMTP_HOST'],
+    port:                 ENV['SMTP_PORT'],
+    user_name:            ENV['SMTP_USER'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   Rails.application.config.middleware.use ExceptionNotification::Rack,
     email: {
       email_prefix: '[HOTELISH] ',
